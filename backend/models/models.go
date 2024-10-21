736b692cadbc3d52a1b3b02bb621fe3a2f0e9fdd
SAGNIK PAL <palsagnik2102@gmail.com>
2024-10-22 01:51:59 +0530

    UPDATE: models, handler, utils worked on

--- backend/models/models.go
+++ backend/models/models.go
@@ -1,14 +1,16 @@
 package models
 
 type User struct {
-	Email   string `json:"email"`
-	Name    string `json:"name"`
-	Number  string `json:"number"`
-	Expense int64  `json:"expense"`
+	Email       string `json:"email"`
+	Name        string `json:"name"`
+	Number      string `json:"number"`
+	Expense     int64  `json:"expense"`
+	Password    string `json:"password"`
+	ConfirmPass string `json:"confirm"`
 }
 
 type Transaction struct {
-	Users     []*User 					 	// Each transaction would have a list of users
-	SplitType string  `json:"splittype"` 	// Type of split taken place in transaction: equal, exact, percentage
-	Total     int64   `json:"total"`		// The total amount in 
+	Users     []*User // Each transaction would have a list of users
+	SplitType string  `json:"splittype"` // Type of split taken place in transaction: equal, exact, percentage
+	Total     int64   `json:"total"`     // The total amount in
 }
