27e011313a4953224e71ef695dc8c7525b4134b3
SAGNIK PAL <palsagnik2102@gmail.com>
2024-10-22 12:06:23 +0530

    UPDATE: database initialisation

---
+++ backend/database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/palSagnik/daily-expenses-application/config"
+)
+
+var (
+	DB *gorm.DB
+	err error
+)
+
+// connecting to the database
+func ConnectDB() error {
+	
+	connectionString := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d", config.DB_HOST, config.DB_USER, config.DB_PASS, config.DB_NAME, config.DB_PORT)
+	DB, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	log.Infof("connected to database: %s\n", config.DB_NAME)
+	
+	return nil
+}
